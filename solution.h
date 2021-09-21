43531caaadc75326d833c6b009a6699b37b8ec8e
Heider Sanchez <48141762+heidersanchez@users.noreply.github.com>
2021-09-21 11:20:22 -0500

    Update solution.h

--- solution.h
+++ solution.h
@@ -1,19 +1,22 @@
-
 #include <iostream>
-#include "stacklist.h"
-#include "forwardlist.h"
 
 using namespace std;
 
-float evaluate(string input)
+struct Result{
+    double result;
+    bool error;
+}
+
+
+Result evaluate(string input)
 {
-    // 1- descomponer el input
+    // 1- descomponer el input y validar
 
     // 2- convertir de Infijo a Postfijo
     
     // 3- resolver la expresion
 
-    //* Si no cumple la validacion lanzar un exception con throw "Error";
+    //* Si no cumple la validacion retornar Result.error = true;
 
-    return 0.0;
+    return Result();
 }
