a7d4a1842f6e39fe3c44753d027af94eea43d0c9
Heider Sanchez <48141762+heidersanchez@users.noreply.github.com>
2023-09-04 11:46:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,16 +9,16 @@ int main()
 {
     string expr="";
     expr = "5 + 8 / 2";
-    ASSERT(evaluate(expr).result == 9, "The function evaluate is not working");
+    ASSERT(evaluate(expr).result == 9 && evaluate(expr).error ==  false, "The function evaluate is not working");
 
     expr = "(7 + 8) / 2";
-    ASSERT(evaluate(expr).result == 7.5, "The function evaluate is not working");
+    ASSERT(evaluate(expr).result == 7.5 && evaluate(expr).error ==  false, "The function evaluate is not working");
 
-    expr = "(6 + 8) / (5 + 2)";
-    ASSERT(evaluate(expr).result == 2, "The function evaluate is not working");
+    expr = "(14 + 7) / (5 + 2)";
+    ASSERT(evaluate(expr).result == 2 && evaluate(expr).error ==  false, "The function evaluate is not working");
 
-    expr = "(6 + 8) / (5 + 2) * 12";
-    ASSERT(evaluate(expr).result == 24, "The function evaluate is not working");
+    expr = "(3 * 2 + 8) / (10 - 5 + 2) * 12";
+    ASSERT(evaluate(expr).result == 24 && evaluate(expr).error ==  false, "The function evaluate is not working");
 
     expr = "(6 + 8 / (5 + 2) * 3";
     ASSERT(evaluate(expr).error == true, "The function evaluate is not working");
@@ -27,6 +27,9 @@ int main()
     ASSERT(evaluate(expr).error == true, "The function evaluate is not working");
 
     expr = "(6 + 8) 10 / (5 + 2) * 3 +";
+    ASSERT(evaluate(expr).error == true, "The function evaluate is not working");
+    
+    expr = "(6 + 8 -) 10 / (5 + 2) * 3 )";
     ASSERT(evaluate(expr).error == true, "The function evaluate is not working");
 
     return  0;  
